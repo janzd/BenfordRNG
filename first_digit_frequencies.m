fcd1e9c87c7ff2bbe33dfd3e72997cc434a6d22a
Jan Zdenek <kuragepantsu@gmail.com>
2015-05-27 03:08:39 +0900

    added function for calculation of frequencies

---
+++ first_digit_frequencies.m
@@ -0,0 +1,15 @@
+function frequencies = first_digit_frequencies(data)
+% Returns the frequencies of first digits in numerical data and
+% plots the result.
+% INPUT:
+%   data - analysed numerical data
+% OUTPUT:
+%   frequencies - frequencies of first digits in data
+
+datastr = arrayfun(@(x) num2str(x), data, 'UniformOutput', false);
+[n, ~] = hist(cellfun(@(x) str2double(x(1)), datastr), 9);
+frequencies = n ./ sum(n);
+bar(1:9, n);
+
+end
+
